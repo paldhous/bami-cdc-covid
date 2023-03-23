201235bacaa0ecdc19b8928c565a64ff7f915305
Peter Aldhous <peter@peteraldhous.com>
2023-03-23 16:11:50 -0700

    sort data

--- R/get_data.R
+++ R/get_data.R
@@ -15,7 +15,8 @@ states_timeline <- read_csv("https://data.cdc.gov/api/views/pwn4-m3yp/rows.csv?a
   clean_names() %>%
   rename(state_postal = state) %>%
   inner_join(populations) %>%
-  select(state, state_postal, week_end_date = end_date, tot_cases, tot_deaths, week_cases = new_cases, week_deaths = new_deaths, population) 
+  select(state, state_postal, week_end_date = end_date, tot_cases, tot_deaths, week_cases = new_cases, week_deaths = new_deaths, population) %>%
+  arrange(state,week_end_date)
 
 # covid latest
 states_latest <- fromJSON("https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=US_MAP_DATA")
@@ -26,12 +27,13 @@ states_latest <- states_latest$US_MAP_DATA %>%
 states_latest <- states_latest %>%
   select(state_postal = abbr,
          week_end_date = us_trend_maxdate,
-         tot_cases, 
-         tot_deaths = tot_death, 
-         week_cases = new_cases07, 
+         tot_cases,
+         tot_deaths = tot_death,
+         week_cases = new_cases07,
          week_deaths = new_deaths07) %>%
   inner_join(populations) %>%
-  select(7,1:6,8)
+  select(7,1:6,8) %>%
+  arrange(state)
 
 # write to csv
 write_csv(states_timeline, "states_timeline.csv", na = "")
